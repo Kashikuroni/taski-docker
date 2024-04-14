9f74b4be7ca9216974f84ab73b497d7bd4296d5a
Никита Тарасов <owlramps@yandex.ru>
2024-04-14 14:21:45 +0300

    create workflows

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['130.193.53.104', 'taskibykshi.bounceme.net']
+ALLOWED_HOSTS = ['localhost', '130.193.53.104', 'kittygramcool.ddns.net']
 
 
 # Application definition
